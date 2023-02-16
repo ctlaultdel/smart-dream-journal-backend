e17fc44bdc0c3fe3946aafb898d5ab3e6f075f06
larissa <larza.ault@gmail.com>
2023-02-16 11:30:38 -0800

    Added validate_model helper function

--- app/models/validation_checkers.py
+++ app/models/validation_checkers.py
@@ -2,6 +2,20 @@ from .User import User
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
+from flask import abort, make_response
+
+def validate_model(cls, model_id):
+    try:
+        model_id = int(model_id)
+    except:
+        # handle invalid entry id type
+        abort(make_response({"message":f"{cls.__name__} {model_id} invalid"}, 400))
+    # return entry data if id in db
+    model = cls.query.get(model_id)
+    # handle nonexistant planet id
+    if not model:
+        abort(make_response({"message":f"{cls.__name__} {model_id} not found"}, 404))
+    return model
 
 class RegisterForm(FlaskForm):
     username = StringField(
