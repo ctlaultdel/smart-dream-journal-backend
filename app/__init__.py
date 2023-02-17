d28c41d3da55a67dedece5f3a7847935879d71f5
larissa <larza.ault@gmail.com>
2023-02-16 18:59:20 -0800

    condensed authentification routes into one blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -31,19 +31,19 @@ def create_app(test_config=None):
     migrate.init_app(app, db)
 
     # register blueprints
-    from app.routes.login_routes import main_bp, login_bp, register_bp
-    from app.routes.users_routes import users_bp
+    from app.routes.login_routes import auth
+    # from app.routes.users_routes import users_bp
     from app.routes.entries import entries_bp
-    from app.routes.dreams101 import dreams101_bp
-    from app.routes.calendar import calendar_bp
-    from app.routes.analyses import analyses_bp
-    app.register_blueprint(main_bp)
-    app.register_blueprint(users_bp)
-    app.register_blueprint(register_bp)
-    app.register_blueprint(login_bp)
+    # from app.routes.dreams101 import dreams101_bp
+    # from app.routes.calendar import calendar_bp
+    # from app.routes.analyses import analyses_bp
+    app.register_blueprint(auth)
+    # app.register_blueprint(users_bp)
+    # app.register_blueprint(register_bp)
+    # app.register_blueprint(login_bp)
     app.register_blueprint(entries_bp)
-    app.register_blueprint(dreams101_bp)
-    app.register_blueprint(calendar_bp)
-    app.register_blueprint(analyses_bp)
+    # app.register_blueprint(dreams101_bp)
+    # app.register_blueprint(calendar_bp)
+    # app.register_blueprint(analyses_bp)
     
     return app
