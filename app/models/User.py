d2247756c704f175e5ad8ac2b73a62da146ac5df
larissa <larza.ault@gmail.com>
2023-02-18 19:35:02 -0800

    Added email attribute to User class

--- app/models/User.py
+++ app/models/User.py
@@ -6,6 +6,7 @@ class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
+    email = db.Column(db.String(120), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
     entries = db.relationship("Entry", back_populates="user")
 
