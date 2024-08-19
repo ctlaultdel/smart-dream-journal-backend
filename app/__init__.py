90acef9ea6eeaded9fd577b78efd82975fb46ab3
larissa <larza.ault@gmail.com>
2024-08-18 18:05:19 -0700

    fixed uri

--- app/__init__.py
+++ app/__init__.py
@@ -19,10 +19,10 @@ def create_app(test_config=None):
 
     # select development or testing db
     if test_config:
-        app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://u7t68trlggdsaf:pf2c8d7c67016b20d7dfca6265c439c5ca7f6dbb302c962498004945624459118@c67okggoj39697.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dplf47a4qqve3"
+        app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://u7t68trlggdsaf:pf2c8d7c67016b20d7dfca6265c439c5ca7f6dbb302c962498004945624459118@c67okggoj39697.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dplf47a4qqve3"
         app.config["TESTING"] = True
     else:
-        app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://u7t68trlggdsaf:pf2c8d7c67016b20d7dfca6265c439c5ca7f6dbb302c962498004945624459118@c67okggoj39697.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dplf47a4qqve3"
+        app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://u7t68trlggdsaf:pf2c8d7c67016b20d7dfca6265c439c5ca7f6dbb302c962498004945624459118@c67okggoj39697.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dplf47a4qqve3"
 
     # import models for Alembic setup
     from app.models.Entry import Entry
